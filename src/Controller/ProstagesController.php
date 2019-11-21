99323b893e902eb5d083b2494a37da49fdf24d41
mchevrier004 <mchevrier004@iutbayonne.univ-pau.fr>
2019-11-21 08:30:14 +0100

    Création des routes

--- src/Controller/ProstagesController.php
+++ src/Controller/ProstagesController.php
@@ -8,12 +8,26 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProstagesController extends AbstractController
 {
     /**
-     * @Route("/prostages", name="prostages")
+     * @Route("/prostages", name="prostagesAccueil")
      */
     public function index()
     {
         return $this->render('prostages/index.html.twig', [
             'controller_name' => 'ProstagesController',
         ]);
+    }
+    /**
+     * @Route("/entreprises", name="prostagesEntreprises")
+     */
+    public function afficherEntreprises()
+    {
+        return $this->render('prostages/entreprises.html.twig', ['controller_name' => 'ProstagesController', ]);
+    }
+    /**
+     * @Route("/formations", name="prostagesFormations")
+     */
+    public function afficherFormations()
+    {
+        return $this->render('prostages/formations.html.twig', ['controller_name' => 'ProstagesController', ]);
     }
 }
