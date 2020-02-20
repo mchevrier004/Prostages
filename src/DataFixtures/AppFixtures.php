8fad49aef7bd512086ee2f48ad63028b41162f81
mchevrier004 <mchevrier004@iutbayonne.univ-pau.fr>
2020-02-20 08:18:33 +0100

    Résolution du problème des fixtures Fakers

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -17,18 +17,18 @@ class AppFixtures extends Fixture
         for($i = 0;$i < 10;$i++){
             $entreprise = new Entreprise();
             $entreprise->setNom($faker->company);
-            $entreprise->setType($faker->randomElement('Développement Web','Programmation informatique'));
+            $entreprise->setType($faker->randomElement(['Développement Web','Programmation informatique']));
             $entreprise->setSite($faker->url);
             $entreprise->setAdresse($faker->address);
             $entreprise->setTel($faker->phoneNumber);
         
             $formation = new Formation();
-            $formation->setNom($faker->randomElement('DUT Informatique','DU TIC','GIM','DUT GEA','LP Info'));
-            $formation->setDiplome($faker->randomElement('Bac+1','Bac+2','Bac+3','Bac+4','Bac+5'));
+            $formation->setNom($faker->randomElement(['DUT Informatique','DU TIC','GIM','DUT GEA','LP Info']));
+            $formation->setDiplome($faker->randomElement(['Bac+1','Bac+2','Bac+3','Bac+4','Bac+5']));
         
             $stage = new OffreStage();
             $stage->setDescription($faker->text(50));
-            $stage->setNom($faker->randomElement('Stage Technicien informatique','Stage développeur web','Stage Symfony'));
+            $stage->setNom($faker->randomElement(['Stage Technicien informatique','Stage développeur web','Stage Symfony']));
             $stage->setMail($faker->email);
             $stage->setTel($faker->phoneNumber);
             $stage->addFormation($formation);
@@ -46,3 +46,4 @@ class AppFixtures extends Fixture
         $manager->flush();
     }
 }
+?>
